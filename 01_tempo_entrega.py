55a442cbdfea5107bece0686df9ec94145fd524a
Fagundes1993 <mfbl93@gmail.com>
2023-08-21 21:43:52 -0300

    first commit

---
+++ 01_tempo_entrega.py
@@ -0,0 +1,8 @@
+nomeRestaurante = input()
+tempoEstimadoEntrega = int(input())
+
+print("O restaurante {} entrega em {} minutos." .format (nomeRestaurante, tempoEstimadoEntrega))  
+
+# Imprimir a saída no padrão definido no enunciado deste desafio.
+# Dica: Para simplificar a formatação, utilize o conceito de interpolação de strings.
+
